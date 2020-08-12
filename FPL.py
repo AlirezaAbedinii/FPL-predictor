345fec43039dd0cf46bd4547f972d2d3de31d32e
mhnaderi99 <mhnaderi99@gmail.com>
2020-08-12 21:05:35 +0430

    get data from FPL API

--- FPL.py
+++ FPL.py
@@ -1,8 +1,17 @@
-###
-print('hello world')
+import requests
+import pandas as pd
+import numpy as np
 
-print('hello from arezz')
-print('mh')
-print('second arezz')
-print('second mh')
+url = 'https://fantasy.premierleague.com/api/bootstrap-static/'
+r = requests.get(url)
+json = r.json()
+print(json.keys())
 
+events_df = pd.DataFrame(json['events'])
+#game_settings_df = pd.DataFrame(json['game_settings']) has error
+phases_df = pd.DataFrame(json['phases'])
+teams_df = pd.DataFrame(json['teams'])
+#total_players_df = pd.DataFrame(json['total_players']) has error
+elements_df = pd.DataFrame(json['elements'])
+element_stats_df = pd.DataFrame(json['element_stats'])
+elements_types_df = pd.DataFrame(json['element_types'])
