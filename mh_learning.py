1605ec9e71ebeb567f0d05770cdb138c23fc5c82
mhnaderi99 <mhnaderi99@gmail.com>
2020-08-16 20:45:07 +0430

    fix some issues

--- mh_learning.py
+++ mh_learning.py
@@ -2,38 +2,26 @@ from sklearn.neural_network import MLPRegressor
 from sklearn.model_selection import train_test_split
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 
 
-def learner(position, unnecessary_data):
+def learner(position, necessary_data):
+
     x_df = pd.read_csv(data_path + 'cumulative_gw_2.csv')
     y_df = pd.read_csv(data_path + 'total_points_gw_2.csv')
     x_df = x_df.loc[x_df['position'] == position]  # select position
     y_df = y_df.loc[y_df['position'] == position]  # select position
 
-    del x_df['position']
-    del y_df['position']
-
-    x_df = x_df.transpose()
-    y_df = y_df.transpose()
-
-    x_df = x_df.iloc[2:]
-    y_df = y_df.iloc[3:]
-
-    x_df = x_df.transpose()
-    y_df = y_df.transpose()
-
+    x_df = x_df[necessary_data]
+    print(x_df)
+    y_df = y_df['total_points']
     x_df['was_home'] = x_df['was_home'].apply(lambda x: 1 if x == True else 0)
-
-    for data in unnecessary_data:
-        del x_df[data]
-    # del x_df['was_home']
+    x_df['minutes']/=90.0
 
     x = x_df.to_numpy()
     y = y_df.to_numpy().reshape(-1)
 
-    x = x.astype(int)
-    y = y.astype(int)
+    x = x.astype(float)
+    y = y.astype(float)
 
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 
@@ -45,11 +33,6 @@ def learner(position, unnecessary_data):
     mse = ((output - y_test) ** 2).mean()
     print('MSE = ' + str(mse))
     non_blank_performance(y_test, output, 6)
-    """""
-    df = pd.DataFrame({'Actual': y_test.flatten(), 'Predicted': output.flatten()})
-    plt.scatter(y_test, output, color='gray', alpha=0.5)
-    plt.show()
-    """
 
 
 def non_blank_performance(real, predicted, threshold):
@@ -74,4 +57,5 @@ def non_blank_performance(real, predicted, threshold):
 
 
 data_path = 'mh_learning_data\\'
-learner(2, ['minutes'])
+data = ['goals_scored', 'assists', 'bonus', 'total_points', 'was_home', 'minutes']
+learner(4, data)
