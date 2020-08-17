29319b627b18155aa5b8c292c16f3d01f80d1202
alireza <alireza.abedini78@gmail.com>
2020-08-17 17:55:40 +0430

    goalkeeper updates

--- mh_learning.py
+++ mh_learning.py
@@ -26,7 +26,10 @@ def learner(position):
     x = x.astype(float)
     y = y.astype(float)
 
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+    if position == 1:
+        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1)
+    else:
+        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 
     if position > 1:
         regressor = MLPRegressor(hidden_layer_sizes=(100, 100), solver='adam' , activation='logistic', max_iter=100000,
@@ -35,13 +38,16 @@ def learner(position):
         # regressor = DecisionTreeRegressor(criterion='mse', splitter='best', max_depth=10, min_samples_split=0.2,
         #                                   min_samples_leaf=1, min_weight_fraction_leaf=0.0,
         #                                   max_features='auto').fit(x_train, y_train)
-        regressor = MLPRegressor(hidden_layer_sizes=(10, 10, 10), solver='adam', activation='logistic', max_iter=100000,
+        regressor = MLPRegressor(hidden_layer_sizes=(20, 20), solver='adam', activation='logistic', max_iter=100000,
                                   learning_rate_init=0.0001, learning_rate='invscaling').fit(x_train, y_train)
+
+
     output = regressor.predict(x_test)
 
     difference = abs(output - y_test)
     print('VARIANCE = ' + str(np.var(difference)))
-    mse = difference.mean()
+    squared_difference = (output - y_test)**2
+    mse = squared_difference.mean()
     print('MSE = ' + str(mse))
     non_blank_performance(y_test, output, 6, 100)
     df = pd.DataFrame({'Actual': y_test.flatten(), 'Predicted': output.flatten()})
@@ -80,8 +86,8 @@ def pie_chart(real, predicted):
 data_path = 'mh_learning_data\\'
 data_per_position = dict()
 data_per_position[4] = ['goals_scored', 'assists', 'opponent_conceded', 'bonus', 'bps', 'form', 'total_points', 'was_home', 'minutes', 'yellow_cards', 'red_cards', 'difficulty']
-data_per_position[3]= ['goals_scored', 'assists', 'bonus', 'form', 'bps', 'total_points', 'was_home', 'minutes', 'yellow_cards', 'red_cards', 'clean_sheets', 'difficulty']
-data_per_position[2] = ['goals_scored', 'assists', 'bonus', 'form', 'total_points', 'was_home', 'minutes', 'yellow_cards', 'red_cards', 'goals_conceded', 'clean_sheets', 'difficulty']
-data_per_position[1] = ['bonus', 'bps', 'form', 'total_points', 'saves', 'goals_conceded', 'was_home', 'clean_sheets', 'yellow_cards', 'red_cards' ,'penalties_saved', 'difficulty']
+data_per_position[3]= ['opponent_conceded','goals_scored', 'assists', 'bonus', 'form', 'bps', 'total_points', 'was_home', 'minutes', 'yellow_cards', 'red_cards', 'clean_sheets', 'difficulty']
+data_per_position[2] = ['opponent_goals', 'goals_scored', 'assists', 'bonus', 'form', 'total_points', 'was_home', 'minutes', 'yellow_cards', 'red_cards', 'goals_conceded', 'clean_sheets', 'difficulty']
+data_per_position[1] = ['opponent_goals', 'bonus', 'bps', 'form', 'total_points', 'saves', 'goals_conceded', 'was_home', 'clean_sheets', 'yellow_cards', 'red_cards' ,'penalties_saved', 'difficulty']
 
-learner(3)
+learner(2)
